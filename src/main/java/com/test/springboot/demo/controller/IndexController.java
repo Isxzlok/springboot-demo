b06c635845ed8637e8449b2e4ece076774ef1c53
熊二 <xiongzhilong@xiongerdeMac-mini.local>
2019-02-20 14:12:16 +0800

    m

--- src/main/java/com/test/springboot/demo/controller/IndexController.java
+++ src/main/java/com/test/springboot/demo/controller/IndexController.java
@@ -18,9 +18,19 @@ public class IndexController {
      *
      * 1. git init
      *
-     * 2. git status //查看项目中有哪些需要提交的文件
-     *
      * 2. git remote add origin https://github.com/Isxzlok/springboot-demo
+     *
+     * 3. git status //查看项目中有哪些需要提交的文件
+     *
+     * 4. git add . //添加所要提交的文件到commit
+     *
+     * 5. git commit -m '这是提交的原因'
+     *
+     * 6. git push --set-upstream origin master //第一次提交敲这条命令
+     *
+     * 以后提交都只需敲 git push
+     *
+     *
      *
      */
 
